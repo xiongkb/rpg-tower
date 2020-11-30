0b3d670e708eb1e651caafe9bc6ccbe7a000a8df
xiongkb <mkan-bx@hotmail.com>
2020-11-30 13:38:38 -0800

    styling with flex on heroes

--- src/App.js
+++ src/App.js
@@ -11,10 +11,8 @@ function App() {
   return (
     <div className="App">
       <h1 className="title">RPG Tower</h1>
-      <div className="dungeon-zone">
-        <div className="display-heroes">
-          {currentParty.map( hero => <HeroCard hero={heroData[hero]}/>)}
-        </div>
+      <div className="display-heroes">
+        {currentParty.map( hero => <HeroCard hero={heroData[hero]}/>)}
       </div>
     </div>
   );
