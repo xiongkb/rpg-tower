27bdc80864760f37e395f1946ff001a19d2fb62f
xiongkb <mkan-bx@hotmail.com>
2020-11-29 19:27:02 -0800

    added labels to stats

--- src/Components/Heroes/heroCard.js
+++ src/Components/Heroes/heroCard.js
@@ -9,14 +9,14 @@ const HeroCard = (props) => {
             <div className="level"><span className="lvl-span">lvl</span>{props.hero.level}</div>
             <img className="hero-pic" alt={props.hero.alt} src={props.hero.img}></img>
             <div className="hero-name">{props.hero.name}</div>
-            <div className="hero-health">{props.hero.stats.hp}</div>
-            <div className="hero-exp">{props.hero.exp}</div>
+            <div className="hero-health"><span>HP</span>{props.hero.stats.hp}</div>
+            <div className="hero-exp"><span>EXP</span>{props.hero.exp}</div>
             <div>
-                <div className="hero-tp">{props.hero.stats.tp}</div>
-                <div className="hero-mp">{props.hero.stats.mp}</div>
+                <div className="hero-tp"><span>TP</span>{props.hero.stats.tp}</div>
+                <div className="hero-mp"><span>MP</span>{props.hero.stats.mp}</div>
             </div>
         </div>
     )
 }
 
-export default HeroCard
+export default HeroCard;
