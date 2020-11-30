d4cfeee7fe248204b95183dfe09d11db6964991c
xiongkb <mkan-bx@hotmail.com>
2020-11-29 19:54:40 -0800

    stylized hero card

--- src/Components/Heroes/heroCard.js
+++ src/Components/Heroes/heroCard.js
@@ -9,11 +9,13 @@ const HeroCard = (props) => {
             <div className="level"><span className="lvl-span">lvl</span>{props.hero.level}</div>
             <img className="hero-pic" alt={props.hero.alt} src={props.hero.img}></img>
             <div className="hero-name">{props.hero.name}</div>
-            <div className="hero-health"><span>HP</span>{props.hero.stats.hp}</div>
-            <div className="hero-exp"><span>EXP</span>{props.hero.exp}</div>
-            <div>
-                <div className="hero-tp"><span>TP</span>{props.hero.stats.tp}</div>
-                <div className="hero-mp"><span>MP</span>{props.hero.stats.mp}</div>
+            <div className="stat-placement">
+                <div className="hero-health"><span className="stat-label">HP</span>{props.hero.stats.hp}</div>
+                <div className="hero-exp"><span className="stat-label">EXP</span>{props.hero.exp}</div>
+            </div>
+            <div className="stat-placement">
+                <div className="hero-tp"><span className="stat-label">TP</span>{props.hero.stats.tp}</div>
+                <div className="hero-mp"><span className="stat-label">MP</span>{props.hero.stats.mp}</div>
             </div>
         </div>
     )
