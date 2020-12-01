661ac08b836674d6bad983ed10d81adc3a4bf1c2
xiongkb <mkan-bx@hotmail.com>
2020-12-01 12:10:21 -0800

    fixed flex box stylings

--- src/Components/Heroes/heroCard.js
+++ src/Components/Heroes/heroCard.js
@@ -6,16 +6,22 @@ const HeroCard = (props) => {
     
     return (
         <div className="hero-card">
-            <div className="level"><span className="lvl-span">lvl</span>{props.hero.level}</div>
-            <img className="hero-pic" alt={props.hero.alt} src={props.hero.img}></img>
-            <div className="hero-name">{props.hero.name}</div>
-            <div className="stat-placement">
-                <div className="hero-health"><span className="stat-label">HP</span>{props.hero.stats.hp}</div>
-                <div className="hero-exp"><span className="stat-label">EXP</span>{props.hero.exp}</div>
+            <div className="display-coloumn">
+                <div className="level"><span className="lvl-span">lvl</span>{props.hero.level}</div>
+                <img className="hero-pic" alt={props.hero.alt} src={props.hero.img}></img>
             </div>
-            <div className="stat-placement">
-                <div className="hero-tp"><span className="stat-label">TP</span>{props.hero.stats.tp}</div>
-                <div className="hero-mp"><span className="stat-label">MP</span>{props.hero.stats.mp}</div>
+            <div className="display-coloumn">
+                <div className="hero-name">{props.hero.name}</div>
+                <div className="display-row">
+                    <div className="stat-placement">
+                        <div className="hero-health"><span className="stat-label">HP</span>{props.hero.stats.hp}</div>
+                        <div className="hero-exp"><span className="stat-label">EXP</span>{props.hero.exp}</div>
+                    </div>
+                    <div className="stat-placement">
+                        <div className="hero-tp"><span className="stat-label">TP</span>{props.hero.stats.tp}</div>
+                        <div className="hero-mp"><span className="stat-label">MP</span>{props.hero.stats.mp}</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
