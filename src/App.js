3d2c57baf579fe38a2f7808199876549a907f7c8
xiongkb <mkan-bx@hotmail.com>
2020-12-05 14:52:37 -0800

    setting up intro page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, {useState, useEffect} from "react"
 import './App.css';
+// components
 import HeroCard from "./Components/Heroes/heroCard"
 import heroData from "./Components/Heroes/heroesData";
 import MonsterCard from "./Components/Enemies/monsterCard";
 import monsterData from "./Components/Enemies/mobData";
+import Intro from "./Components/Intro/intro"
 
 function App() {
+  // introduction to game
+  const [displayIntro, setDisplayIntro] = useState(true);
   // playable characters
-  const [currentParty, setCurrentParty] = useState([]);
+  const [currentParty, setCurrentParty] = useState(["warrior"]);
   // monster enemies
   const [currentEnemies, setCurrentEnemies] = useState([]);
+  // current floor
+  const [currentFloor, setCurrentFloor] = useState();
+
+  // map objects
   let monsterArr = Object.keys(monsterData);
   let heroArr = Object.keys(heroData);
 
-
+  // hero party generator variables
   let chosenHeroArr = [];
   let heroIndex = []
+  let hPartySize = 0;
 
+  // setting up floor level
+  
+  // determining monster party size upon reaching to a floor
   useEffect(() => {
     let mPartySize = Math.floor(Math.random() * 6);
-    let hPartySize = 0;
     let chosenMonArr = [];
     // selecting monsters party
     for (let i=0; i < mPartySize; i++) {
@@ -30,16 +41,17 @@ function App() {
     setCurrentEnemies(chosenMonArr);
 
     // a hero can join is monster party is 0
-    if (mPartySize === 0 && hPartySize < 3) {
-      let ranHeroIndex = Math.floor(Math.random() * 3);
-      console.log("hero index: " + heroIndex);
+    if (mPartySize === 0) { //  && hPartySize < 3
+      console.log("A hero either joins the party or you recieve a blessing")
+      // let ranHeroIndex = Math.floor(Math.random() * 3);
+      // console.log("hero index: " + heroIndex);
 
-      if (!heroIndex.includes(ranHeroIndex)) {
-        chosenHeroArr.push(heroArr[ranHeroIndex]);
-        console.log("random hero: " + ranHeroIndex);
+      // if (!heroIndex.includes(ranHeroIndex)) {
+      //   chosenHeroArr.push(heroArr[ranHeroIndex]);
+      //   console.log("hero index: " + ranHeroIndex);
         
-      };
-      setCurrentParty(chosenHeroArr);
+      // };
+      // setCurrentParty(chosenHeroArr);
     };
     
     console.log("chosen hero: " + chosenHeroArr);
@@ -53,13 +65,18 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="title">RPG Tower</h1>
-      <div className="display-monsters">
-        {currentEnemies.map( monster => <MonsterCard monster={monsterData[monster]}/>)}
-      </div>
-      <div className="display-heroes">
-        {currentParty.map( hero => <HeroCard hero={heroData[hero]}/>)}
-      </div>
+      {displayIntro ? <Intro onClick={false}/> : 
+        <div>
+          <h1 className="title">RPG Tower</h1>
+          <div>Current Floor: {currentFloor}</div>
+          <div className="display-monsters">
+            {currentEnemies.map( monster => <MonsterCard monster={monsterData[monster]}/>)}
+          </div>
+          <div className="display-heroes">
+            {currentParty.map( hero => <HeroCard hero={heroData[hero]}/>)}
+          </div>
+        </div>
+      }
     </div>
   );
 }
---
+++ src/Components/Intro/intro.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./intro.css"
+
+const intro = () => {
+    return (
+        <div>
+            <h4>Welcome to RPG Tower</h4>
+            <p>
+                This will show you a quick tutorial on how to play.
+            </p>
+        </div>
+    )
+}
+
+export default intro;
