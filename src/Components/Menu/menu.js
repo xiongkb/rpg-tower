5c52997d03c955007f339c94229f959f0f0d23f1
xiongkb <mkan-bx@hotmail.com>
2020-12-08 11:48:30 -0800

    stylized menu buttons

--- src/Components/Menu/menu.js
+++ src/Components/Menu/menu.js
@@ -5,6 +5,7 @@ const Menu = () => {
     return (
         <div className="menu-box">
             <div className="menu-item">Attack</div>
+            <hr className="hr-border"></hr>
             <div className="menu-item">Run</div>
         </div>
     )
